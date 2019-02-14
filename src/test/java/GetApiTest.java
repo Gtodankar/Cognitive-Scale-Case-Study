b113c4d04b7afcb36384f9c47d8c60b50d056125
gtodankar <girijatodankar02@gmail.com>
2019-02-14 11:57:29 +0530

    added updated src code

--- src/test/java/GetApiTest.java
+++ src/test/java/GetApiTest.java
@@ -18,19 +18,25 @@ public class GetApiTest {
                 and().
                 contentType(ContentType.JSON).
                 and().
-                time(lessThan(30000L));
+                time(lessThan(3000L));
 
         System.out.println("Response data is :" + response.asString());
         System.out.println("Response Time in ms :" + response.getTime());
 
-        Assert.assertEquals(response.getBody().asString().toLowerCase().contains("site_admin"), true);
+
+        Assert.assertEquals(response.getBody().asString().toLowerCase().contains("location"), true);
+        System.out.println("location present" );
 
         // First get the JsonPath object instance from the Response interface
         JsonPath jsonPath = response.jsonPath();
         int id = jsonPath.get("id");
-        System.out.println("User ID received from Response " + id);
+        System.out.println("User ID received from Response : " + id);
         Assert.assertEquals(id, 583231);
+
+        String location=jsonPath.get("location");
+        Assert.assertEquals(location, "San Francisco");
+        System.out.println("User location received from Response : " + location);
     }
 
-    
+
 }
